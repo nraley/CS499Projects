a4cfc9ea304c974b491b959294de4de0f0bfc54f
Christopher Renfro <46904903+ThadChunder@users.noreply.github.com>
2020-10-08 20:54:24 -0400

    Add files via upload
    saw that sounds just good enough

--- project2/BlitSaw.java
+++ project2/BlitSaw.java
@@ -1,4 +1,4 @@
-public class BlitSaw extends Blit 
+/*public class BlitSaw extends Blit 
     {
     // this might be useful
     double prev = 0;
@@ -23,3 +23,28 @@ public class BlitSaw extends Blit
         return val;
         }
     }
+*/
+
+public class BlitSaw extends Blit 
+    {
+    // this might be useful
+    double prev = 0;
+
+	public double blitsaw(long tickCount)
+		{
+		double bs = Utils.valueToHz(getFrequencyMod().getValue());
+		bs  = Math.min(Math.max(1.0, bs), Config.NYQUIST_LIMIT);
+		return bs;
+		}
+		
+    public double tick(long tickCount) 
+        {
+        /// IMPLEMENT ME.  Note how the other implementations of tick worked
+        double P  = Config.SAMPLING_RATE / blitsaw(tickCount);
+        double alpha = 1 - 1/P;
+        double val = alpha*prev + super.tick(tickCount) - 1/P;
+        prev = val;
+        val += 0.5;
+        return val;
+        }
+	}
