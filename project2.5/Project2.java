4c3ee4283a6330829c6fc44bf997acc032e70b2c
Nelson <nelson@raleygroup.com>
2020-10-22 23:08:19 -0400

    Implemented baseline p2.5 file, created window and UI elements, with p2 blit and ADSR

--- project2.5/Project2.java
+++ project2.5/Project2.java
@@ -14,15 +14,116 @@ public class Project2 extends Synth
     	// Implement me:
     	
     	// Build a MidiModule
-    	// Build a MidiGate
-    	// Create the window
+		MidiModule midimod = new MidiModule(getMidi());
+		modules.add(midimod);
+		// Build a MidiGate
+		MidiGate gate = new MidiGate(midimod);
+		modules.add(gate);
+		// Create the window
+		JFrame frame = new JFrame();
+		Box outer = new Box(BoxLayout.X_AXIS);
+		frame.setContentPane(outer);
     	// Build the oscillators
+
+        //Blit oscillators
+
+        Box blitBox = new Box(BoxLayout.Y_AXIS);
+        blitBox.setBorder(BorderFactory.createTitledBorder("Blits"));
+        outer.add(blitBox);
+
+        Blit blit = new Blit();
+        BlitSaw blitSaw = new BlitSaw();
+        BlitSquare blitSqr = new BlitSquare();
+        BPBlit bpbLit = new BPBlit();
+        BlitTriangle blitTri = new BlitTriangle();
+
     	// Build dials for the amplitude of each of the oscillators, plus
     	//       the pulse width of the square and triangle
-    	// Put them in a box and put it in the window
+        // Put them in a box and put it in the window
+
+        //Blit dials
+
+        Dial blitAmp = new Dial(0.0);
+        blitBox.add(blitAmp.getLabelledDial("Blit Amplitude"));
+
+        Dial bpblitAmp = new Dial(0.0);
+        blitBox.add(bpblitAmp.getLabelledDial("BPBlit Amplitude"));
+
+        Dial sawAmp = new Dial(1.0);
+        blitBox.add(sawAmp.getLabelledDial("Saw Amplitude"));
+
+        Dial sqrAmp = new Dial(0.0);
+        blitBox.add(sqrAmp.getLabelledDial("Square Amplitude"));
+
+        Dial sqrPulse = new Dial(0.5);
+        blitBox.add(sqrPulse.getLabelledDial("Square Pulse Width"));
+
+        Dial triAmp = new Dial(0.0);
+        blitBox.add(triAmp.getLabelledDial("Triangle Amplitude"));
+
+        Dial triPulse = new Dial(0.5);
+        blitBox.add(triPulse.getLabelledDial("Triangle Pulse Width"));
+
+        //box for envelope
+        Box centerBox = new Box(BoxLayout.Y_AXIS);	//center box to hold Env
+        outer.add(centerBox);
+
+        Box envBox = new Box(BoxLayout.Y_AXIS);
+        envBox.setBorder(BorderFactory.createTitledBorder("Env"));
+        centerBox.add(envBox);
+
+        Dial attackDial = new Dial(0.1);
+        envBox.add(attackDial.getLabelledDial("Attack Time"));
+
+        Dial decayDial = new Dial(0);
+        envBox.add(decayDial.getLabelledDial("Decay Time"));
+
+        Dial sustainDial = new Dial(1.0);
+        envBox.add(sustainDial.getLabelledDial("Sustain"));
+
+        Dial releaseDial = new Dial(0.1);
+        envBox.add(releaseDial.getLabelledDial("Release Time"));
+
+        blitSqr.setPhaseMod(sqrPulse.getModule());
+        blitTri.setPhaseMod(triPulse.getModule());
+
+        modules.add(blit);
+        modules.add(blitSqr);
+        modules.add(blitSaw);
+        modules.add(blitTri);
+        modules.add(bpbLit);
+
     	// Build a mixer for the various oscillators
-    	
-    	/// NEW IN PROJECT 2.5
+
+        Mixer mixer = new Mixer(7);	//7 oscillators for the mixer
+        mixer.setInput(0, blit);
+        mixer.setInput(1, blitSaw);
+        mixer.setInput(2, blitSqr);
+        mixer.setInput(3, blitTri);
+        mixer.setInput(4, bpbLit);
+        mixer.setAmplitudeMod(0, blitAmp.getModule());
+        mixer.setAmplitudeMod(1, sawAmp.getModule());
+        mixer.setAmplitudeMod(2, sqrAmp.getModule());
+        mixer.setAmplitudeMod(3, triAmp.getModule());
+        mixer.setAmplitudeMod(4, bpblitAmp.getModule());
+        blit.setFrequencyMod(midimod);
+        blitSaw.setFrequencyMod(midimod);
+        blitSqr.setFrequencyMod(midimod);
+        blitTri.setFrequencyMod(midimod);
+        bpbLit.setFrequencyMod(midimod);
+
+        modules.add(mixer);
+
+        // Build an ADSR
+        ADSR adsr = new ADSR();
+        adsr.setAttackTime(attackDial.getModule());
+        adsr.setDecayTime(decayDial.getModule());
+        adsr.setSustainLevel(sustainDial.getModule());
+        adsr.setReleaseTime(releaseDial.getModule());
+        adsr.setGate(gate);
+        modules.add(adsr);
+
+        /// NEW IN PROJECT 2.5
     	// Build an ADSR for the filter
     	// Build dials for the ADSR and put it in a box
     	// Build dials for the Cutoff and Resonance of the filter
@@ -30,14 +131,35 @@ public class Project2 extends Synth
     	// Build a Mul to multiply the cutoff by the ADSR output
     	// Feed the Mul as the cutoff for a LOW PASS FILTER (also feed in the resonance)
     	// END NEW IN PROJECT 2.5
-    	
-    	// Build an ADSR for the VCA
-    	// Build a VCA controlled by the ADSR which gets its input from the FILTER <--- NOTE CHANGE
-    	// Make an output box
-    	// Add a Gain amplifier
-    	// Add an oscilloscope
-    	// Set the output to the gain amplifier (or oscilloscope)
-    	// Pack and display the window
+
+        // Build a VCA controlled by the ADSR which gets its input from the mixer
+        Amplifier AAA = new Amplifier();
+        AAA.setAmplitudeMod(adsr);
+        AAA.setInput(mixer);
+        modules.add(AAA);
+        // Make an output box
+        Box outputBox = new Box(BoxLayout.Y_AXIS);
+        outputBox.setBorder(BorderFactory.createTitledBorder("Output"));
+        outer.add(outputBox);
+
+        Dial dial = new Dial(1.0);
+        outputBox.add(dial.getLabelledDial("Gain"));
+        // Add a Gain amplifier
+        Amplifier gainAmp = new Amplifier();
+        gainAmp.setInput(AAA);
+        gainAmp.setAmplitudeMod(dial.getModule());
+        modules.add(gainAmp);
+        // Add an oscilloscope
+        Oscilloscope oscope = new Oscilloscope();
+        Oscilloscope.OModule omodule = oscope.getModule();
+        modules.add(omodule);
+        omodule.setAmplitudeModule(gainAmp);
+        outputBox.add(oscope);
+        // Set the output to the gain amplifier (or oscilloscope)
+        setOutput(omodule);
+        // Pack and display the window
+        frame.pack();
+        frame.setVisible(true);
         }
         
     }
